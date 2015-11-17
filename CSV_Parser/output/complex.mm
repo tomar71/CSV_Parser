7272e16b13e3aa062dbc579f11c54960e96c001e
tomar71 <tariqaliomar@gmail.com>
2015-11-16 21:30:01 -0500

    Complex output of mind map generator.

---
+++ CSV_Parser/output/complex.mm
@@ -0,0 +1,124 @@
+<map version="0.9.0" >
+<node TEXT="ROOT" >
+    <node TEXT="eType" >
+        <attribute NAME="att1"  VALUE="PERSON" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="id" />
+        <attribute NAME="att2"  VALUE="UINT" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="idStr" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="firstName" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="lastName" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="father" />
+        <attribute NAME="att2"  VALUE="ID" />
+        <attribute NAME="att3"  VALUE="PERSON" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="mother" />
+        <attribute NAME="att2"  VALUE="ID" />
+        <attribute NAME="att3"  VALUE="PERSON" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="cars" />
+        <attribute NAME="att2"  VALUE="ARRAY" />
+        <attribute NAME="att3"  VALUE="ID" />
+        <attribute NAME="att4"  VALUE="CAR" />
+    </node>
+    <node TEXT="PERSON" >
+        <attribute NAME="att1"  VALUE="0" />
+        <attribute NAME="att2"  VALUE="P0" />
+        <attribute NAME="att3"  VALUE="John" />
+        <attribute NAME="att4"  VALUE="root" />
+        <attribute NAME="att5"  VALUE="" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+    <node TEXT="PERSON" >
+        <attribute NAME="att1"  VALUE="1" />
+        <attribute NAME="att2"  VALUE="P1" />
+        <attribute NAME="att3"  VALUE="Jane" />
+        <attribute NAME="att4"  VALUE="root" />
+        <attribute NAME="att5"  VALUE="" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+    <node TEXT="PERSON" >
+        <attribute NAME="att1"  VALUE="2" />
+        <attribute NAME="att2"  VALUE="P2" />
+        <attribute NAME="att3"  VALUE="Jack" />
+        <attribute NAME="att4"  VALUE="root" />
+        <attribute NAME="att5"  VALUE="0" />
+        <attribute NAME="att6"  VALUE="1" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="1" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="3" />
+    </node>
+    <node TEXT="eType" >
+        <attribute NAME="att1"  VALUE="CAR" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="id" />
+        <attribute NAME="att2"  VALUE="UINT" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="idStr" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="make" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="model" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="att" >
+        <attribute NAME="att1"  VALUE="numberPlate" />
+        <attribute NAME="att2"  VALUE="STRING" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="0" />
+        <attribute NAME="att2"  VALUE="C0" />
+        <attribute NAME="att3"  VALUE="toyota" />
+        <attribute NAME="att4"  VALUE="corolla" />
+        <attribute NAME="att5"  VALUE="X1256" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="1" />
+        <attribute NAME="att2"  VALUE="C1" />
+        <attribute NAME="att3"  VALUE="ford" />
+        <attribute NAME="att4"  VALUE="escort" />
+        <attribute NAME="att5"  VALUE="A1525" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="2" />
+        <attribute NAME="att2"  VALUE="C2" />
+        <attribute NAME="att3"  VALUE="kia" />
+        <attribute NAME="att4"  VALUE="pride" />
+        <attribute NAME="att5"  VALUE="M1546" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+    <node TEXT="CAR" >
+        <attribute NAME="att1"  VALUE="3" />
+        <attribute NAME="att2"  VALUE="C3" />
+        <attribute NAME="att3"  VALUE="nissan" />
+        <attribute NAME="att4"  VALUE="datsun" />
+        <attribute NAME="att5"  VALUE="I5648" />
+        <attribute NAME="att6"  VALUE="" />
+    </node>
+</node>
+</map>
